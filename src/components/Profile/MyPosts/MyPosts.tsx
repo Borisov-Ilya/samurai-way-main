81b85886b48b9cd3966a1019a8dfa52a3ef8498b
Borisov-Ilya <borisov.ilya22@gmail.com>
2022-03-27 14:49:57 +0300

    24

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,11 @@ import s from './MyPosts.module.css';
 import {Post} from './Post/Post';
 
 export const MyPosts = () => {
+    let postsData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11},
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +20,8 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={'Hi, how are you?'} likesCount={30}/>
-                <Post message={'It\'s my first post'} likesCount={15}/>
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
             </div>
         </div>
     );
